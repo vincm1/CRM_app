d718b00952bfa03fc686f84a0724b3633bf540b0
Vincent Michler vincent.michler94@gmail.com <vincent.michler94@gmail.com>
2020-11-25 16:23:15 +0100

    Update and delete functions

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
 from .models import *
 from .forms import OrderForm
 
@@ -33,25 +34,40 @@ def customer(request, pk):
   context = {'customer': customer, 'orders': orders, 'orders_count': orders_count}
   return render(request, 'accounts/customer.html', context)
 
-def createOrder(request):
-
-  form = OrderForm()
+def createOrder(request, pk):
+  OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status',), extra=5)
+  customer = Customer.objects.get(id=pk)
+  formset = OrderFormSet(queryset=Order.objects.none(),instance=customer)
+  #form = OrderForm(initial= {'customer': customer})
   if request.method == 'POST':
     #print('Printing POST:', request.POST)
-    form = OrderForm(request.POST)
-    if form.is_valid():
-        form.save()
+    #form = OrderForm(request.POST)
+    formset = OrderFormSet(request.POST, instance=customer)
+    if formset.is_valid():
+        formset.save()
         return redirect('/')
+
+  context = {'formset': formset}
+  return render(request, 'accounts/order_form.html', context)
+
+def updateOrder(request, pk):
+  order = Order.objects.get(id=pk)
+  form = OrderForm(instance=order)
+
+  if request.method == 'POST':
+    form = OrderForm(request.POST, instance=order)
+    if form.is_valid():
+      form.save()
+      return redirect('/')
 
   context = {'form': form}
   return render(request, 'accounts/order_form.html', context)
 
-def updateOrder(request, pk):
-
+def deleteOrder(request, pk):
   order = Order.objects.get(id=pk)
-  form =  OrderForm()
-  if form.is_valid():
-        form.save()
-        return redirect('/')
-  context = {'form':form}
-  return render(request, 'accounts/order_form.html')
+  if request.method == "POST":
+    order.delete()
+    return redirect('/')
+
+  context = {'item':order}
+  return render(request, 'accounts/delete.html', context)
