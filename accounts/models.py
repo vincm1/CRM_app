69fc5fae5aad85f65a3ba7305347b5f8ff111043
Vincent Michler vincent.michler94@gmail.com <vincent.michler94@gmail.com>
2020-12-03 21:49:34 +0100

    user customer connection

--- accounts/models.py
+++ accounts/models.py
@@ -1,16 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Customer(models.Model):
+  user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
   name = models.CharField(max_length=200, null=True)
   phone = models.CharField(max_length=200, null=True)
   email = models.CharField(max_length=200, null=True)
   date_created = models.DateTimeField(auto_now_add=True, null=True)
 
-
   def __str__(self):
     return self.name
+
 
 class Tag(models.Model):
   name = models.CharField(max_length=200, null=True)
@@ -45,6 +47,7 @@ class Order(models.Model):
   product = models.ForeignKey(Product, null=True, on_delete= models.SET_NULL)
   date_created = models.DateTimeField(auto_now_add=True, null=True)
   status = models.CharField(max_length=200, null=True, choices=STATUS)
+  note = models.CharField(max_length=1000, null=True)
 
   def __str__(self):
-      return self.product.name
+    return self.product.name
--- accounts/views.py
+++ accounts/views.py
@@ -77,8 +77,13 @@ def home(request):
 
   return render(request, 'accounts/dashboard.html', context)
 
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
 def userPage(request):
-  context = {}
+  orders = request.user.customer.order_set.all()
+  print('ORDERS',orders)
+
+  context = {'orders': orders}
   return render(request, 'accounts/user.html', context)
 
 
